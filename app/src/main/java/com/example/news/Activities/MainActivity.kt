61972ebec4277e4626b8232da0cd38ec899c614f
Tejas-67 <tejasjha3210@gmail.com>
2023-04-06 23:05:49 +0530

    BreakingNews Fragment implemented

--- app/src/main/java/com/example/news/Activities/MainActivity.kt
+++ app/src/main/java/com/example/news/Activities/MainActivity.kt
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.news.Database.ArticleDatabase
 import com.example.news.Fragments.BreakingNewsFragment
 import com.example.news.Fragments.SavedNewsFragment
 import com.example.news.Fragments.SearchNewsFragment
+import com.example.news.NewsRepository.NewsRepository
 import com.example.news.R
+import com.example.news.UI.NewsViewModel
+import com.example.news.UI.NewsViewModelProviderFactory
 import com.example.news.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationBarView
 
@@ -15,23 +20,32 @@ import com.google.android.material.navigation.NavigationBarView
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding?=null
     private val binding get()=_binding!!
+    public lateinit var viewModel : NewsViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.w("TEJAS", "MainActivity")
 
         _binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setCurrentFragment(BreakingNewsFragment())
 
+        val bnf=BreakingNewsFragment()
+        val snf=SearchNewsFragment()
+        val sanf=SavedNewsFragment()
+        setCurrentFragment(bnf)
         binding.bottomNavView.setOnItemSelectedListener {
                 when(it.itemId){
-                    R.id.savedNewsFragment->setCurrentFragment(SavedNewsFragment())
-                        R.id.searchNewsFragment-> setCurrentFragment(SearchNewsFragment())
-                    R.id.breakingNewsFragment->setCurrentFragment(BreakingNewsFragment())
+                    R.id.savedNewsFragment->setCurrentFragment(sanf)
+                        R.id.searchNewsFragment-> setCurrentFragment(snf)
+                    R.id.breakingNewsFragment->setCurrentFragment(bnf)
 
                 }
             true
         }
+
+        val repo = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(repo)
+        viewModel=ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
+
 
     }
 
